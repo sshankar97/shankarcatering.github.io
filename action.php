9fc1c6f61bc4b37825f33cfcc7943091059532af
shankarcatering <37026346+shankarcatering@users.noreply.github.com>
2019-01-13 19:34:39 -0500

    Update action.php

--- action.php
+++ action.php
@@ -1,6 +1,5 @@
 <?php
-//if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
+
   
   //Email information
   $admin_email = "mashankarcatering@gmail.com";
@@ -13,10 +12,6 @@
   
   //Email response
   echo "Thank you for contacting us! You'll here back from us soon!";
-  }
-  
-  //if "email" variable is not filled out, display the form
-  else  {
-    echo "Please try again";
-  }
+
+
 ?>
