606f7e9727decf64425f1fe1e832d16335b12f79
shankarcatering <37026346+shankarcatering@users.noreply.github.com>
2019-01-13 19:26:19 -0500

    Create action.php

---
+++ action.php
@@ -0,0 +1,20 @@
+<?php
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "mashankarcatering@gmail.com";
+  $email = $_REQUEST['email'];
+  $name = $_REQUEST['name'];
+  $message = $_REQUEST['message'];
+  
+  //send email
+  mail($admin_email, "$name", $message, "From:" . $email);
+  
+  //Email response
+  echo "Thank you for contacting us! You'll here back from us soon!";
+  }
+  
+  //if "email" variable is not filled out, display the form
+  else  {
+?>
